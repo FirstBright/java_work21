a58cb25e27134475752bbb038b1dfb41dd6aa91b
dron512 <parkmyounghoi@gmail.com>
2021-03-08 09:57:24 +0900

    no message

--- 20210302_1/src/Main.java
+++ 20210302_1/src/Main.java
@@ -13,11 +13,26 @@ public class Main {
 	 * 5. 학생 파일로 저장
 	 */
 	
+	public static Student inputStudent(){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("이름은? ");
+		String name = scan.nextLine();
+		System.out.println("성별은? ");
+		String gender = scan.nextLine();
+		System.out.println("나이?");
+		int age = scan.nextInt();
+		Student stu = new Student(name, gender, age);
+		return stu;
+	}
 	public static void main(String[] args) {
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		String a = br.readLine();
-		
+		// C#, java, python...
+		// c malloc free 동적으로 메모리할당...
+		// ArrayList... Haspmap...
+		// python... 배열...동적으로 메모리할당..
 		Student[] student = new Student[50];
+//		System.out.println("student[0]="+student[0]);
+//		System.out.println("student[1]="+student[1]);
+//		System.out.println("student[2]="+student[2]);
 		
 		Scanner scan = new Scanner(System.in);
 		while(true){
@@ -26,24 +41,40 @@ public class Main {
 			System.out.println("select = "+ select);
 			if(select ==1){
 				//학생등록
-				scan.nextLine();
-				System.out.println("이름은? ");
-				String name = scan.nextLine();
-				System.out.println("성별은? ");
-				String gender = scan.nextLine();
-				System.out.println("나이?");
-				int age = scan.nextInt();
-				student[0] = new Student(name, gender, age);
+				Student stu = inputStudent();
+				for(int i = 0; i< student.length ; i++){
+					if( student[i] == null ){
+						student[i] = stu;
+						break;
+					}
+				}
 			}
 			else if(select==2)
 			{
 				System.out.println("===학생조회===");
 				for(int i=0; i<student.length;i++){
-					System.out.println(i+" = "+student[i]);
+//					System.out.println(i+" = "+student[i]);
 					if(student[i] != null)
 						student[i].print();
 				}
 				System.out.println("============");
+			}
+			else if(select==3)
+			{
+				System.out.println("수정하고 싶은 학생 번호 입력 : ");
+				int num = scan.nextInt();
+				
+				Student stu = inputStudent();
+				student[num] = stu;
+				
+				System.out.println("수정되었습니다.");
+			}
+			else if(select==4)
+			{
+				System.out.println("삭제하고 싶은 학생 번호 입력 : ");
+				int num = scan.nextInt();
+				student[num] = null;
+				System.out.println("삭제되었습니다.");
 			}
 			else{
 				System.out.println("종료됩니다.");
